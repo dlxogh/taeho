ba32c0ef215b833f41d1b66a98b344d9f70317f1
DATA8320-08 <DATA8320-08@DESKTOP-D6DEL7J>
2020-05-13 17:02:50 +0900

    과제파일

--- 2project/src/java2.java
+++ 2project/src/java2.java
@@ -1,9 +1,10 @@
+import java.awt.datatransfer.SystemFlavorMap;
 
 public class java2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       
+           System.out.println("¾È³ç java");
 	}
 
 }
