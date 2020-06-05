4e8c76c3538b31cb2229407721166efc68e055ca
DATA8320-08 <taehoop1215@naver.com>
2020-06-05 09:28:06 +0900

    구구단 while문

--- do/src/Main.java
+++ do/src/Main.java
@@ -1,27 +1,20 @@
-import java.util.Scanner;
-
-public class Main {
-       public static void main(String args[]){
-    	 
-    	   Scanner sc = new Scanner(System.in);
-         	   
-    	   int a = sc.nextInt();
-    	   int b = sc.nextInt();
-    	   int c = sc.nextInt();
-    
-    	   
-    	 
-    	   System.out.println((a+b)%c  );
-    	   System.out.println((a%c+b%c)%c   );
-    	   
-    	   
-    	   System.out.println((a*b)%c   );
-    	   System.out.println((a%c*b%c)%c   );
-    	  sc.close();
-              	   
-    	      	
-    	  
-  	   
-    	   
-       }
-       }
+public class Main      {
+        public static void main(String[] args) {                     
+        
+           int i=3;int j=1;
+            int count=0;
+            while(i<=8) {
+               if((i==3||i==5)&&j==1)
+                  System.out.println("<"+i+"´Ü>");
+               
+            
+            System.out.println(i+"*"+j+"="+i*j);
+            j++;
+            count+=1;
+            if(count%9==0) {
+               i++;
+               j-=9;
+            }
+            }
+        }
+}            
