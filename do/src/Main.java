9805a01f1d677be229dd4ab630e13fe14c9992bc
DATA8320-08 <taehoop1215@naver.com>
2020-06-10 12:52:08 +0900

    시험성적 계산

--- do/src/Main.java
+++ do/src/Main.java
@@ -1,20 +1,6 @@
-public class Main      {
-        public static void main(String[] args) {                     
-        
-           int i=3;int j=1;
-            int count=0;
-            while(i<=8) {
-               if((i==3||i==5)&&j==1)
-                  System.out.println("<"+i+"´Ü>");
-               
-            
-            System.out.println(i+"*"+j+"="+i*j);
-            j++;
-            count+=1;
-            if(count%9==0) {
-               i++;
-               j-=9;
-            }
-            }
-        }
-}            
+public class Main {
+	public static void main(String [] args) {
+		
+		
+	}
+}
