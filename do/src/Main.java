eb15d7719e5da9f9fad970d3775dad78a78ef78c
DATA8320-08 <taehoop1215@naver.com>
2020-06-11 09:01:08 +0900

    세자리수 곱하기

--- do/src/Main.java
+++ do/src/Main.java
@@ -1,7 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
-	 public static void main (String [] args) {
-		            System.out.println(");
-	 }
-	 }
+       public static void main(String args[]){
+    	 
+    	   Scanner s = new Scanner(System.in);
+         	   
+    	   int a = s.nextInt();
+    	   int b = s.nextInt();
+    	 
+    
+    	   
+    	 
+    	   System.out.println(a*(b%10));
+    	   System.out.println(a*((b%100)/10));
+    	   System.out.println(a*(b/100));
+    	   System.out.println(a * b );
+    	 
+              	   
+    	      	
+    	  
+  	   
+    	   
+       }
+       }
